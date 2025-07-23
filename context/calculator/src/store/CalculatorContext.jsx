d0aec7855fc7c422e12b0096b2aacbab9e2399fe
Avinash <avinashrana55@yahoo.com>
2025-07-23 15:04:54 +0530

    Global state manipulation

--- context/calculator/src/store/CalculatorContext.jsx
+++ context/calculator/src/store/CalculatorContext.jsx
@@ -2,23 +2,54 @@ import { createContext, useState } from "react";
 
 export const CalculatorContext = createContext();
 
-export const CalculatorContextProvider = ({children}) => {
-    const [inputData, setInputData] = useState({
-        num1 : "",
-        num2 : "",
-    })
+export const CalculatorContextProvider = ({ children }) => {
+  const [inputData, setInputData] = useState({
+    num1: "",
+    num2: "",
+  });
 
-    const handleInputChange = (e) => {
-        setInputData((prev) => ({...prev, [e.target.name] : parseFloat(e.target.value)}));
-        //console.log(inputData)
-    }
+  const [result, setResult] = useState(0);
 
-    console.log(inputData, handleInputChange)
-    return(
-        <CalculatorContext.Provider value={{inputData, handleInputChange}}>
-            {children}
-        </CalculatorContext.Provider>
-    )
+  const handleInputChange = (e) => {
+    setInputData((prev) => ({
+      ...prev,
+      [e.target.name]: parseFloat(e.target.value),
+    }));
+    //console.log(inputData)
+  };
+
+  const calculateAddition = () => {
+    setResult(inputData.num1 + inputData.num2);
+  };
+
+  const calculateDeletion = () => {
+    setResult(inputData.num1 - inputData.num2);
+  };
+
+  const calculateMultiplication = () => {
+    setResult(inputData.num1 * inputData.num2);
+  };
+
+  const calculateDivision = () => {
+    setResult(inputData.num1 / inputData.num2);
+  };
+
+  //console.log(inputData, handleInputChange)
+  return (
+    <CalculatorContext.Provider
+      value={{
+        inputData,
+        result,
+        handleInputChange,
+        calculateAddition,
+        calculateDeletion,
+        calculateDivision,
+        calculateMultiplication,
+      }}
+    >
+      {children}
+    </CalculatorContext.Provider>
+  );
 };
 
 //export default CalculatorContextProvider;

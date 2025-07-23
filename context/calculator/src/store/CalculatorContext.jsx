6a4d407977a9f6727c56dd728dfbb1fe79fd7959
Avinash <avinashrana55@yahoo.com>
2025-07-23 12:26:33 +0530

    Handling input data in global state

--- context/calculator/src/store/CalculatorContext.jsx
+++ context/calculator/src/store/CalculatorContext.jsx
@@ -1,11 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const CalculatorContext = createContext();
 
 export const CalculatorContextProvider = ({children}) => {
-    let str = "Hello World!";
+    const [inputData, setInputData] = useState({
+        num1 : "",
+        num2 : "",
+    })
+
+    const handleInputChange = (e) => {
+        setInputData((prev) => ({...prev, [e.target.name] : parseFloat(e.target.value)}));
+        //console.log(inputData)
+    }
+
+    console.log(inputData, handleInputChange)
     return(
-        <CalculatorContext.Provider value={str}>
+        <CalculatorContext.Provider value={{inputData, handleInputChange}}>
             {children}
         </CalculatorContext.Provider>
     )

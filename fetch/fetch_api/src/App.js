21b5f186797a6630c1701cb9190e706469c94536
Avinash <avinashrana55@yahoo.com>
2025-07-29 16:46:18 +0530

    Added PUT request in fetch

--- fetch/fetch_api/src/App.js
+++ fetch/fetch_api/src/App.js
@@ -10,8 +10,6 @@ function App() {
       .then((data) => setUsers(data.users));
   }, []);
 
-  console.log(users);
-
   const handleAddUser = () => {
     fetch("https://dummyjson.com/users/add", {
       method: "POST",
@@ -25,14 +23,35 @@ function App() {
       .then((data) => console.log(data));
   };
 
+  const handleEditUser = () => {
+    fetch("https://dummyjson.com/users/1", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstName: "Avi",
+        lastName: "Rana",
+      }),
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+  };
+
   return (
     <div className="max-w-[600px] mx-auto space-y-2 my-5 m-2">
       <button
         type="button"
-        className="border bg-gray-100 px-3 py-1 text-xs"
+        className="border bg-gray-100 px-3 py-1 text-xs mr-4"
         onClick={handleAddUser}
       >
         Add User
+      </button>
+
+      <button
+        type="button"
+        className="border bg-red-200 px-3 py-1 text-xs"
+        onClick={handleEditUser}
+      >
+        Edit
       </button>
       {users.map((user) => (
         <div className="p-2 border border-gray-100 rounded-md" key={user.id}>

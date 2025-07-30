b8f882ff8b8557475791f0e476c3b838d55bf25b
Avinash <avinashrana55@yahoo.com>
2025-07-30 13:25:06 +0530

    Used dotenv for environment variables and added PORT there

--- Node/express/index.js
+++ Node/express/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 
+const PORT = process.env.PORT;
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`Listening to the port 3000`)
+app.listen(PORT, () => {
+    console.log(`Listening to the port ${PORT}`)
 });

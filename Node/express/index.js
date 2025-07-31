d38d2292152b5f5389f74c9fc65d3195dce26ca3
Avinash <avinashrana55@yahoo.com>
2025-07-31 17:52:51 +0530

    Created a get route

--- Node/express/index.js
+++ Node/express/index.js
@@ -4,6 +4,15 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
+app.get("/", (req, res) => {
+    const data = {
+        firstName : "Avinash",
+        lastName : "Rana",
+        age : 30
+    }
+    res.status(200).send(data);
+});
+
 app.listen(PORT, () => {
     console.log(`Listening to the port ${PORT}`)
 });

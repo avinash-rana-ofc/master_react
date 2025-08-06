794b9486fa5d5f9401c4532326c10a7b76f3b047
Avinash <avinashrana55@yahoo.com>
2025-08-06 12:30:58 +0530

    Establishing pg connection or else shwing error

--- Node/express/index.js
+++ Node/express/index.js
@@ -18,16 +18,14 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.get("/", async(req, res) => {
-  const client = await pool.connect();
-  try {
-    const result = await client.query('SELECT NOW()');
-    console.log(result.rows[0]);
-    res.status(200).json({"Status" : 200, "Message": "connection successful"});
-  } catch (error) {
-    console.log("Query error", error.stack);
-  } finally{
-    client.release();
-  }
+  pool.connect((err, client, release) => {
+    if(err){
+      res.status(500).send(err);
+    }
+
+    res.status(200).send("Connection Established");
+  });
+  
 });
 
 app.listen(PORT, () => {

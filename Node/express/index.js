2808111c7043601a6fd5c2e5eb94204979a0fce9
Avinash <avinashrana55@yahoo.com>
2025-08-07 18:23:23 +0530

    Added add route and added insert query

--- Node/express/index.js
+++ Node/express/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+
 const products = require("./routes/products");
 
 const PORT = process.env.PORT;
 
-
+app.use(express.json());
 app.use("/products", products);
 
 
--- Node/express/routes/products.js
+++ Node/express/routes/products.js
@@ -10,19 +10,35 @@ const pool = new Pool({
   port: 5432,
 });
 
-
 const PORT = process.env.PORT;
 const router = express.Router();
 
-//test connection
+//get products
 router.get("/", async (req, res) => {
-  const rows = await pool.query("SELECT * FROM products", (err, result) => {
-    if(err){
+  await pool.query("SELECT * FROM products", (err, result) => {
+    if (err) {
       res.status(500).send(err);
     }
 
     res.status(200).send(result.rows);
-  })
+  });
+});
+
+//add product
+router.post("/add", async (req, res) => {
+  const { image, name, price, description } = req.body;
+  console.log(image, name, price, description);
+  try {
+    const results = await pool.query(
+      "INSERT INTO products (image, name, price, description) VALUES ( $1, $2, $3, $4) RETURNING *",
+      [image, name, price, description]
+    );
+    res.status(201).send(results.rows[0]);
+  } catch (error) {
+    if (err) {
+      res.status(500).send({ error: err });
+    }
+  }
 });
 
 module.exports = router;

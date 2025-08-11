d50ad52cfff7fba7b223ed25eedfe8589229854e
Avinash <avinashrana55@yahoo.com>
2025-08-11 17:22:44 +0530

    Update all database variables to .env

--- Node/express/routes/products.js
+++ Node/express/routes/products.js
@@ -1,13 +1,13 @@
 const express = require("express");
-require("dotenv").config();
+//require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "root",
-  database: "local_db",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 const PORT = process.env.PORT;

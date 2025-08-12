89822610441bc9c49c1b9afffd7fc96ccfca1128
Avinash <avinashrana55@yahoo.com>
2025-08-12 16:54:43 +0530

    Added fetch api and handled using useEffect hooke to update on load

--- Routing/understandingroute/src/components/Layout.jsx
+++ Routing/understandingroute/src/components/Layout.jsx
@@ -5,13 +5,13 @@ const Layout = () => {
     <>
       <ul className="flex gap-4 bg-neutral-50 shadow-md p-5 font-semibold">
         <li>
-          <NavLink to="/" className={({isActive}) => isActive && "text-blue-500 font-bold"}>Home</NavLink>
+          <NavLink to="/" className={({isActive}) => isActive ? "text-blue-500 font-bold": ""}>Home</NavLink>
         </li>
         {/* <li>
-          <NavLink to="/products" className={({isActive}) => isActive && "text-blue-500 font-bold"}>Products</NavLink>
+          <NavLink to="/products" className={({isActive}) => isActive ? "text-blue-500 font-bold": ""}>Products</NavLink>
         </li> */}
         <li>
-          <NavLink to="/about" className={({isActive}) => isActive && "text-blue-500 font-bold"}>About</NavLink>
+          <NavLink to="/about" className={({isActive}) => isActive ? "text-blue-500 font-bold": ""}>About</NavLink>
         </li>
       </ul>
       <Outlet />
